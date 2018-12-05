16855f2f8649375d7e20964b409627a54fbc59cb
Chetani Mesa <32041936+chetanimg@users.noreply.github.com>
2018-12-05 03:20:33 +0000

    Update Memoization.h

--- P3/C/Memoization.h
+++ P3/C/Memoization.h
@@ -1,6 +1,7 @@
 #ifndef _Memoization_h
 #define _Memoization_h
 void printKeyValue( gpointer key, gpointer value, gpointer userData ) ;
+void mi_strcpy (char* s1, char* s2, int pos);
 int LOOKUPCHAIN(int array[], int i, int j);
 void clean();
 void initMemoization(int array[], int size);

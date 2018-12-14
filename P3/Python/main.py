f8fd4cfd945070a17628e47128909191884988f9
Ian Marrero <32128820+Ian-M-M@users.noreply.github.com>
2018-12-14 12:54:51 +0000

    Comentado

--- P3/Python/main.py
+++ P3/Python/main.py
@@ -15,17 +15,19 @@ args, flagF, flagT, flagDi, flagDo, flagMem, flagTab = etc.ProcesaArgumentos(fla
 path = args.file
 
 fichero = etc.abrirFichero(path)
-
+# Opcion de mostrar el contenido del archivo que se pasa por parametro
 if flagDi == 1:
     print "File name: " + path + "\n"
     for linea in fichero:
         print linea
     etc.cerrarFichero(fichero)
 
+# Comprobamos errores
 if (flagT == 0) and (flagDi == 0) and (flagDo == 0):
     print "Para que el programa funciones correctemente use alguno de los comandos disponibles, 'python main.py -h'"
     exit(1)
 
+# Comprobamos errores
 if (flagDo == 0 or flagT == 0) and flagTab == 0 and flagMem == 0 and flagDi == 0:
     print "Para mostrar el output ha de indicar que metodo quiere aplicar, indique -mem o -tab"
     exit(1)
@@ -39,15 +41,16 @@ if flagDo == 1 or flagT == 1:
 
         arr = etc.lstStringToInt(linea)
         size = len(arr)
-
+        # Necesitamos al menos 2 matrices
         if size < 3:
             print "El array no es valido, minimo necesito 3 elementos"
         else:
+            # minimo de operaciones y tiempo de tabulation y memoization
             minTab = 0
             minMem = 0
             timeTab = 0
             timeMem = 0
-
+            # Inicializamos el dictionary que vamos a usar para resolver por memoization
             if flagMem == 1:
                 memoization.clean()
                 memoization.init(arr)
@@ -55,31 +58,27 @@ if flagDo == 1 or flagT == 1:
             print "n matrices => " + str(size-1)
 
             if flagTab == 1:
-
+                # Primera medida de tiempo
                 if flagT == 1: etc.time.clock()
 
                 minTab = tabulation.MatrixChainOrder(arr, size)
-
+                # Segunda medida de tiempo
                 if flagT == 1: timeTab = etc.time.clock()
 
             if flagMem == 1:
-
+                # Primera medida de tiempo
                 if flagT == 1: etc.time.clock()
 
                 minMem = memoization.MatrixChainOrder(1, size-1)
-
+                # Segunda medida de tiempo
                 if flagT == 1: timeMem = etc.time.clock()
-
+            # Creamos el output
             if flagDo == 1:
                 if flagTab == 1: print "minimo de operaciones (Tabulation) => " + str(minTab)
                 if flagMem == 1: print "minimo de operaciones (Memoization) => " + str(minMem)
             if flagT == 1:
                 if flagTab == 1: print "Tiempo (Tabulation) => " + str(timeTab) + " s"
                 if flagMem == 1: print "Tiempo (Memoization) => " + str(timeMem) + " s"
-            if timeTab > 120 or timeMem > 120:
-                print "Max time"
-                exit 0
-
         print "--------------------------------------------------------"
 
 etc.cerrarFichero(fichero)
--- P3/Python/memoization.py
+++ P3/Python/memoization.py
@@ -4,6 +4,10 @@ mem = dict()
 inf = sys.maxint
 mat = list
 
+"""
+    init => funcion que inicializa el dictionary
+"""
+
 
 def init(p):
 
@@ -15,21 +19,33 @@ def init(p):
         for j in range(1, n):
 
             key = str(i) + "," + str(j)
-
+            # En la 'diagonal' colocamos 0
             if i == j: mem.update({key: 0})
+            # El resto lo llenamos del valor maximo del integer
             else: mem.update({key: inf})
+
+
+"""
+    clean => funcion que vacia el dictionary
+"""
 
 
 def clean():
     mem.clear()
 
 
+"""
+    MatrixChainOrder => funcion que contiene el algoritmo de memoization
+"""
+
+
 def MatrixChainOrder(i, j):
-
+    # Creamos la key del dictionary
     key = str(i) + "," + str(j)
-
+    # Comprobamos si ya hemos calculado el valor para dicha key
     if mem[key] < inf: return mem[key]
     else:
+        # Calculamos el valor de la key
         for k in range(i, j):
 
             q = MatrixChainOrder(i, k) + MatrixChainOrder(k + 1, j) + mat[i - 1] * mat[k] * mat[j]
